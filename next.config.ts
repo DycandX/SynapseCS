1dc159e3b4fbbd45a48b3a157ea825ab90562919
DycandX <karismaviki@gmail.com>
2026-06-19 10:39:23 +0700

    update next.config.ts use basepath

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 🚀 SOLUSI UTAMA: Beritahu Next.js bahwa aplikasi ini berjalan di sub-folder /synapse-cs
+  // 🚀 Tetap pertahankan ini agar aset internal /_next/static tidak pecah
   basePath: "/synapse-cs",
-
-  // Catatan: Kamu bisa menghapus atau mengomentari baris assetPrefix di bawah ini 
-  // karena basePath secara otomatis menangani perutean aset statis dengan aman.
-  // assetPrefix: process.env.NODE_ENV === "production" ? "https://synapse-cs.vercel.app" : undefined,
 };
 
 export default nextConfig;
